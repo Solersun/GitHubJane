3fa8e6884b8d7a3ef6f31c0db3110efc3e403666
wangjie <347887102@qq.com>
2016-02-26 14:25:28 +0800

    work2 创建 Dog类

---
+++ GitDemo/GitDemo/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 汪杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 汪杰. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
